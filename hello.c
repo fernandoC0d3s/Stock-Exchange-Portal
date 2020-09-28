c1ba87f096f7f468de8404ba14469e3ffac94a3b
fernandoC0d3s <fernandoC0d3s@users.noreply.github.com>
2020-09-28 01:11:26 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -4,9 +4,9 @@
 
 int main(void)
 {
+    //Asks user for input of name
     string name = get_string("What is your name?\n");
+    //takes input and says hello based on input
     printf("Hello world %s", name);
-    
-    
 
 }
