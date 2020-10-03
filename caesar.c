07e78d8308dd9c25af88deec5d37bf55b112be60
fernandoC0d3s <fernandoC0d3s@users.noreply.github.com>
2020-10-03 00:17:45 +0000

    automated commit by submit50

--- caesar.c
+++ caesar.c
@@ -5,42 +5,46 @@
 #include <string.h>
 
 bool iskeyValid(string s);
-int main(int argc, string argv[] )
+int main(int argc, string argv[])
 {
-    if(argc != 2 || !iskeyValid(argv[1]))
+    if (argc != 2 || !iskeyValid(argv[1]))
     {
        printf("Usage: ./caesar key\n");
        return 1;
     }
     int key = atoi(argv[1]);
-    string regularText = get_string("plaintext: ");
+    string regularText = get_string("plaintext: "); //first text 
     
-    int lengthOfText = strlen(regularText);
+    int lengthOfText = strlen(regularText); //length
     printf("ciphertext: ");
     
-    for(int i = 0; i < lengthOfText;i++)
+    for (int i = 0; i < lengthOfText; i++)
     {
-        char c = regularText[i];
+        char c = regularText[i]; 
         
-        if(isalpha(c))
+        if (isalpha(c))
         {
             char m = 'A';
-            if(islower(c))
+            if (islower(c))
+            {
                 m = 'a';
+            }
         
-            printf("%c", (c - m + key) % 26 + m);
+            printf("%c", (c - m + key) % 26 + m); //formula adapted for special cases
         }
         else
-            printf("%c", c);
+        {
+            printf("%c", c); //else ignoring other characters to stay the same aKa spaces
+        }
     }
     printf("\n");
     
 }
-bool iskeyValid(string s)
+bool iskeyValid(string s) //checks if key is valid
 {
     int length = strlen(s);
     for (int i = 0; i < length; i++)
-        if(!isdigit(s[i]))
+        if (!isdigit(s[i]))
             return false;
 
     return true; 
