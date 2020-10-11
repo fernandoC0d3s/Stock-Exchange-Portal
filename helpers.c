8db776ba13fe5ec0359c5c67692f24ded1bfeb3a
fernandoC0d3s <fernandoC0d3s@users.noreply.github.com>
2020-10-11 17:57:28 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -1,19 +1,19 @@
 #include "helpers.h"
 #include <math.h>
-
+#include <stdio.h>
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     
     
     
-    for(int row = 0; row <= height; row++)
+    for(int row = 0; row < height; row++)
     {
-        for(int column = 0; column <= width; column++)
+        for(int column = 0; column < width; column++)
         {
-            int colorRed = image[row][column].rgbtRed;
-            int colorGreen = image[row][column].rgbtGreen;
-            int colorBlue = image[row][column].rgbtBlue;
+            float colorRed = image[row][column].rgbtRed;
+            float colorGreen = image[row][column].rgbtGreen;
+            float colorBlue = image[row][column].rgbtBlue;
             
             
             double average = (colorRed + colorGreen + colorBlue) / 3;
@@ -22,6 +22,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             image[row][column].rgbtRed = average;
             image[row][column].rgbtGreen = average;
             image[row][column].rgbtBlue = average;
+            
+           
             
             
             
