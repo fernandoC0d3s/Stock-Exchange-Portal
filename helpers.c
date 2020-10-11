f53efbe347b287ac991ababd0459b26e8dc17aeb
fernandoC0d3s <fernandoC0d3s@users.noreply.github.com>
2020-10-11 18:25:26 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -28,16 +28,36 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int row = 0; row < height; row++)
+    for (int row = 0; row < height; row++)
     {
-        for(int column = 0; column < width; column++)
+        for (int column = 0; column < width; column++)
         {
             float colorRed = image[row][column].rgbtRed;
             float colorGreen = image[row][column].rgbtGreen;
             float colorBlue = image[row][column].rgbtBlue;
-            int sepiaRed = round(.393 * colorRed + .769 * colorGreen + .189 * colorBlue);
-            int sepiaGreen = round(.349 * colorRed + .686 * colorGreen + .168 * colorBlue);
-            int sepiaBlue = round(.272 * colorRed + .534 * colorGreen + .131 * colorBlue);
+            
+            
+            double testSepiaRed = .393 * colorRed + .769 * colorGreen + .189 * colorBlue;
+            double testSepiaGreen = .349 * colorRed + .686 * colorGreen + .168 * colorBlue;
+            double testSepiaBlue = .272 * colorRed + .534 * colorGreen + .131 * colorBlue;
+          
+            int sepiaRed = round(testSepiaRed);
+            int sepiaGreen = round(testSepiaGreen);
+            int sepiaBlue = round(testSepiaBlue);
+            
+            if (sepiaRed >= 255)
+            {
+                sepiaRed = 255;
+            }
+            if (sepiaGreen >= 255)
+            {
+                sepiaGreen = 255;
+            }
+            if (sepiaBlue >= 255)
+            {
+                sepiaBlue = 255;
+            }
+            
             
             image[row][column].rgbtRed = sepiaRed;
             image[row][column].rgbtGreen = sepiaGreen;
