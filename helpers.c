5dc2a8ef5c78857e4180a2c144a4c01dcf638567
fernandoC0d3s <fernandoC0d3s@users.noreply.github.com>
2020-10-11 18:18:40 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -22,12 +22,31 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
   
         }
     }
-    return ;
+    return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int row = 0; row < height; row++)
+    {
+        for(int column = 0; column < width; column++)
+        {
+            float colorRed = image[row][column].rgbtRed;
+            float colorGreen = image[row][column].rgbtGreen;
+            float colorBlue = image[row][column].rgbtBlue;
+            int sepiaRed = round(.393 * colorRed + .769 * colorGreen + .189 * colorBlue);
+            int sepiaGreen = round(.349 * colorRed + .686 * colorGreen + .168 * colorBlue);
+            int sepiaBlue = round(.272 * colorRed + .534 * colorGreen + .131 * colorBlue);
+            
+            image[row][column].rgbtRed = sepiaRed;
+            image[row][column].rgbtGreen = sepiaGreen;
+            image[row][column].rgbtBlue = sepiaBlue;
+        }
+    }    
+    
+    
+    
     return;
 }
 
