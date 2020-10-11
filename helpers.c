0c8aea656f3f39c835bb8ad0340ee8ef28767898
fernandoC0d3s <fernandoC0d3s@users.noreply.github.com>
2020-10-11 19:18:12 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -74,14 +74,14 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
      for(int row = 0; row < height; row++)
     {
-        for(int column = 0; column < width -1; column++)
+        for(int column = 0; column < width -2; column++)
         {
             int counter = 0;
             counter--;
             
             RGBTRIPLE first = image[row][column];  //FIRST PIXEL
             RGBTRIPLE temp = image[row][column];
-            RGBTRIPLE last = image[row][width - counter]; // last pixel in array
+            RGBTRIPLE last = image[row][width - counter ]; // last pixel in array
             first = last; //make pixel first same to last one
             last = temp; // make last pixel first using temporary value
           
