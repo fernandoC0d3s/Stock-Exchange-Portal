d39ead0c7fd59b6da50e151da60b7f1fcfbf6bf2
fernandoC0d3s <fernandoC0d3s@users.noreply.github.com>
2020-10-13 17:13:28 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -11,15 +11,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             float colorRed = image[row][column].rgbtRed;
             float colorGreen = image[row][column].rgbtGreen;
             float colorBlue = image[row][column].rgbtBlue;
-            
-            
+
+
             double average = (colorRed + colorGreen + colorBlue) / 3;
             int roundedValue = round(average);
-            
+
             image[row][column].rgbtRed = roundedValue;
             image[row][column].rgbtGreen = roundedValue;
             image[row][column].rgbtBlue = roundedValue;
-  
+
         }
     }
     return;
@@ -35,16 +35,15 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             float colorRed = image[row][column].rgbtRed;
             float colorGreen = image[row][column].rgbtGreen;
             float colorBlue = image[row][column].rgbtBlue;
-            
-            
+
             double testSepiaRed = .393 * colorRed + .769 * colorGreen + .189 * colorBlue;
             double testSepiaGreen = .349 * colorRed + .686 * colorGreen + .168 * colorBlue;
             double testSepiaBlue = .272 * colorRed + .534 * colorGreen + .131 * colorBlue;
-          
+
             int sepiaRed = round(testSepiaRed);
             int sepiaGreen = round(testSepiaGreen);
             int sepiaBlue = round(testSepiaBlue);
-            
+
             if (sepiaRed >= 255)
             {
                 sepiaRed = 255;
@@ -57,15 +56,13 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             {
                 sepiaBlue = 255;
             }
-            
+
             image[row][column].rgbtRed = sepiaRed;
             image[row][column].rgbtGreen = sepiaGreen;
             image[row][column].rgbtBlue = sepiaBlue;
         }
-    }    
-    
-    
-    
+    }
+
     return;
 }
 
@@ -73,32 +70,117 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     int realWidth = width /2; // one less comaprison so it should run faster (hyptho)
-     for(int row = 0; row < height; row++)
+     for (int row = 0; row < height; row++)
     {
-        for(int column = 0; column < realWidth; column++)
+        for (int column = 0; column < realWidth; column++)
         {
             float colorRed = image[row][column].rgbtRed;
             float colorGreen = image[row][column].rgbtGreen;
             float colorBlue = image[row][column].rgbtBlue;
-            
-            
+
             image[row][column].rgbtRed =  image[row][width-column -1].rgbtRed;
             image[row][column].rgbtGreen = image[row][width-column -1].rgbtGreen;
             image[row][column].rgbtBlue = image[row][width-column -1].rgbtBlue;
-            
+
             image[row][width-column -1].rgbtRed = colorRed;
             image[row][width-column -1].rgbtGreen = colorGreen;
             image[row][width-column -1].rgbtBlue = colorBlue;
-            
-            
-            
+
         }
-    }    
+    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int row = 0; 0 < height; row++)
+    {
+        for (int column = 0; 0 < width; column++)
+        {
+            int count = 0;
+            double sumRed, sumGreen, sumBlue;
+            //current box
+            if(image[row][column].rgbtRed != NULL)
+            {
+                sumRed += image[row][column].rgbtRed;
+                sumGreen += image[row][column].rgbtGreen;
+                sumBlue += image[row][column].rgbtBlue;
+                count++;
+            }    
+             if(image[row][column+1].rgbtRed != NULL)
+            {
+                sumRed += image[row][column+1].rgbtRed;
+                sumGreen += image[row][column+1].rgbtGreen;
+                sumBlue += image[row][column+1].rgbtBlue;
+                count++;
+            }    
+             if(image[row][column-1].rgbtRed != NULL)
+            {
+                sumRed += image[row][column-1].rgbtRed;
+                sumGreen += image[row][column-1].rgbtGreen;
+                sumBlue += image[row][column-1].rgbtBlue;
+                count++;
+            }    
+            
+            //we start with row on top
+            //directly on top
+            if(image[row +1 ][column].rgbtRed != NULL)
+            {
+                sumRed += image[row +1][column].rgbtRed;
+                sumGreen += image[row+1][column].rgbtGreen;
+                sumBlue += image[row+1][column].rgbtBlue;
+                count++;
+            }
+            //left top row
+            if(image[row +1 ][column -1].rgbtRed != NULL)
+            {
+                sumRed += image[row +1][column-1].rgbtRed;
+                sumGreen += image[row+1][column-1].rgbtGreen;
+                sumBlue += image[row+1][column-1].rgbtBlue;
+                count++;
+            }
+            if(image[row +1 ][column +1].rgbtRed != NULL)
+            {
+                sumRed += image[row +1][column+1].rgbtRed;
+                sumGreen += image[row+1][column+1].rgbtGreen;
+                sumBlue += image[row+1][column+1].rgbtBlue;
+                count++;
+            }
+            if(image[row -1 ][column].rgbtRed != NULL)
+            {
+                sumRed += image[row -1][column].rgbtRed;
+                sumGreen += image[row-1][column].rgbtGreen;
+                sumBlue += image[row-1][column].rgbtBlue;
+                count++;
+            }
+            if(image[row -1 ][column+1].rgbtRed != NULL)
+            {
+                sumRed += image[row -1][column+1].rgbtRed;
+                sumGreen += image[row-1][column+1].rgbtGreen;
+                sumBlue += image[row-1][column+1].rgbtBlue;
+                count++;
+            }
+            if(image[row -1 ][column-1].rgbtRed != NULL)
+            {
+                sumRed += image[row -1][column-1].rgbtRed;
+                sumGreen += image[row-1][column-1].rgbtGreen;
+                sumBlue += image[row-1][column-1].rgbtBlue;
+                count++;
+            }
+            double averageRed = sumRed / count;
+            double averageGreen = sumGreen / count;
+            double averageBlue = sumBlue / count;
+            image[row][column].rgbtRed = averageRed ;
+            image[row][column].rgbtGreen = averageGreen;
+            image[row][column].rgbtBlue = averageBlue;
+            
+        
+
+            
+            
+            
+        }
+    }
     return;
 }
