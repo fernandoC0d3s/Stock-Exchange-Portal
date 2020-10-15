2f6803055eeab68596c52d6dd3c33d91b5f4a2b5
fernandoC0d3s <fernandoC0d3s@users.noreply.github.com>
2020-10-15 02:27:28 +0000

    automated commit by submit50

--- dictionary.c
+++ dictionary.c
@@ -1,5 +1,4 @@
 // Implements a dictionary's functionality
-
 #include <stdbool.h>
 #include "dictionary.h"
 #include <stdio.h>
@@ -17,12 +16,9 @@ typedef struct node
     struct node *next;
 }
 node;
-
 // Number of buckets in hash table
-const unsigned int N = 100000;
-
+const unsigned int N = 111011;
 unsigned int word_count;
-
 unsigned int hash_value;
 
 
@@ -32,9 +28,7 @@ node *table[N];
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    int key = hash(word);
-    
-    
+    int key = hash(word); 
     node* ptr = table[key];
     
     while (ptr != NULL)
@@ -48,7 +42,6 @@ bool check(const char *word)
     }
     return false;
 }
-
 // Hashes word to a number
 unsigned int hash (const char *word)
 {
@@ -56,7 +49,7 @@ unsigned int hash (const char *word)
     int c;
     while ((c = toupper(*word++)))
     {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c *///(DJb2
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c *///(DJb2) uses this << as it denotes noation to shifting bits
     }
     return hash % N;
 }    
@@ -73,13 +66,11 @@ bool load(const char *dictionary)
         printf("Could not open file %s",dictionary);
         return false;
     }    
-    
-    
     char tempWord[LENGTH +1]; //size of word basically
     
     while (fscanf(file,"%s\n", tempWord) != EOF)
     {
-       node* t = malloc(sizeof(node)); // allocates memory of node
+       node* t = malloc(sizeof(node)); // allocates memory of temp node
     
        strcpy(t -> word,tempWord);
        
@@ -98,61 +89,21 @@ bool load(const char *dictionary)
             table[hashValue] = t;
        }
        
-       
        word_count++;
-      
-      
-      
-        
     }
      fclose(file);
      return true;
-    
-    
-    //read strings from file one at a time
-    //fscanf(reads the the scan )
-    
-    //while loop until fscanf == eof 
-    
-    //f(scanf(file, %s, word))
-    
-    
-    //create a new node for each word
-    
-    //malloc check if null after 
-    
-    //hash word to obtain a hash value
-    
-    //node *list == malloc(sizeofNode(list))
-    //
-    
-    //insert node into hash at the location
-    //allocates memory
-    //node*n = malloc(sizeof(node));
-    //strcopy(n->word, "hello");
-    // n-> next =null;
-    //to insert to list(n -> next = list)
-    // list =n
-    
-    
-    
-   
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODo
-    // Check if there are any words
     return word_count;
-    
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // TODO
-    
    for(int i = 0;  i < N; i++)
    {
        node* nodePtr = table[i]; /// temporary node to table head
@@ -160,13 +111,9 @@ bool unload(void)
        {
          node* temp = nodePtr;    /// 
          nodePtr = nodePtr->next;  // node leading to theend
-         free(temp);
-        
+         free(temp); //we free it
        }
-       table[i] = NULL;
+       table[i] = NULL;  //if is alrady lost GG
    }
-    
-    
-    
     return true;
 }
